20d8a51073c356372ee4b89dc16fa31cd5b137a2
Damien Tan <dtjldamien@gmail.com>
2019-06-29 11:25:07 +0800

    edited payment process for cleaner firebase database

---
+++ app/src/main/java/com/example/orbital2019catch/Credit.java
@@ -0,0 +1,59 @@
+package com.example.orbital2019catch;
+
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.example.orbital2019catch.loginandregister.UserProfile;
+import com.example.orbital2019catch.survey.SurveyFirebaseUniqlo;
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Credit {
+
+    private double amount;
+    private double balance;
+    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    public Credit(double amount) {
+        this.amount = amount;
+    }
+
+    public void addCredits() {
+        getBalance();
+        DatabaseReference databaseReference = mDatabase.getReference(mAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                balance = userProfile.getBalance();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        DatabaseReference balanceRef = mDatabase.getReference(mAuth.getUid()).child("balance");
+        balanceRef.setValue(balance + amount);
+    }
+
+    private void getBalance() {
+        DatabaseReference databaseReference = mDatabase.getReference(mAuth.getUid());
+        databaseReference.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                balance = userProfile.getBalance();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+}
