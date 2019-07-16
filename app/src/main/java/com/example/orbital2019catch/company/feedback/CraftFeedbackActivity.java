b7548cd5a10b81cc51007df59cbf174618205b27
Damien Tan <dtjldamien@gmail.com>
2019-07-16 23:42:54 +0800

    designed crafting feedback xml

---
+++ app/src/main/java/com/example/orbital2019catch/company/feedback/CraftFeedbackActivity.java
@@ -0,0 +1,22 @@
+package com.example.orbital2019catch.company.feedback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.orbital2019catch.R;
+
+public class CraftFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_craft_feedback);
+
+        // need company, user email, payout, question, location marker? or qr code?
+    }
+
+    public void onRadioButtonClicked(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/orbital2019catch/company/survey/CraftSurveyActivity.java
@@ -0,0 +1,16 @@
+package com.example.orbital2019catch.company.survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.orbital2019catch.R;
+
+public class CraftSurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_craft_survey);
+    }
+}
