6b7a2ceb460ac41c62e9fd85e68dd032cf287375
cfdkdcftw <chinnfang@gmail.com>
2019-07-27 02:34:10 +0800

    added filter by brand category function to surveys page

--- app/src/main/java/com/example/orbital2019catch/personal/survey/Survey.java
+++ app/src/main/java/com/example/orbital2019catch/personal/survey/Survey.java
@@ -5,19 +5,20 @@ import java.util.List;
 
 public class Survey {
     private String name, expiry, brand;
-    private List<String> usersWhoCompleted;
+    private List<String> usersWhoCompleted, categories;
     private double cashout;
     private long max, curr;
     private Date expiryDF;
 
     public Survey () {}
 
-    public Survey(String name, String expiry, double cashout, String brand, List<String> usersWhoCompleted, long max, long curr, Date expiryDF) {
+    public Survey(String name, String expiry, double cashout, String brand, List<String> usersWhoCompleted, List<String> categories, long max, long curr, Date expiryDF) {
         this.name = name;
         this.expiry = expiry;
         this.cashout = cashout;
         this.brand = brand;
         this.usersWhoCompleted = usersWhoCompleted;
+        this.categories = categories;
         this.max = max;
         this.curr = curr;
         this.expiryDF = expiryDF;
@@ -50,4 +51,6 @@ public class Survey {
     public List<String> getUsersWhoCompleted() { return usersWhoCompleted; }
 
     public Date getExpiryDF() { return expiryDF; }
+
+    public List<String> getCategories() { return categories; }
 }
--- app/src/main/java/com/example/orbital2019catch/personal/survey/SurveysHomeActivity.java
+++ app/src/main/java/com/example/orbital2019catch/personal/survey/SurveysHomeActivity.java
@@ -1,9 +1,11 @@
 package com.example.orbital2019catch.personal.survey;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,10 +17,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.orbital2019catch.personal.MainActivity;
 import com.example.orbital2019catch.R;
@@ -38,16 +42,25 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import javax.annotation.Nullable;
 
 public class SurveysHomeActivity extends AppCompatActivity implements View.OnClickListener {
     Button brandFilter, expiryFilter, payoutFilter;
+    ImageView detailedFilter;
+    String[] categories;
+    Integer[] categoryIndexes;
+    boolean[] checkedCategories;
+    ArrayList<Integer> userSelectedSurveys;
+
     public static int BRAND_FILTER_STATE = 0, EXPIRY_FILTER_STATE = 0, PAYOUT_FILTER_STATE = 0;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private ArrayList<Survey> surveys;
+    private ArrayList<Survey> originalSurveys;
+    private ArrayList<Survey> currSurveys;
     RecyclerView recyclerView;
     EditText searchBar;
 
@@ -61,11 +74,13 @@ public class SurveysHomeActivity extends AppCompatActivity implements View.OnCli
         db.collection("surveys").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                surveys = new ArrayList<>();
+                originalSurveys = new ArrayList<>();
+                currSurveys = new ArrayList<>();
                 for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
-                    surveys.add(snapshot.toObject(Survey.class));
+                    originalSurveys.add(snapshot.toObject(Survey.class));
+                    currSurveys.add(snapshot.toObject(Survey.class));
                 }
-                SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(surveys);
+                SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(currSurveys);
                 recyclerView.setAdapter(surveyAdapterClass);
             }
         });
@@ -89,67 +104,134 @@ public class SurveysHomeActivity extends AppCompatActivity implements View.OnCli
         brandFilter = (Button) findViewById(R.id.brandNameFilterBtn);
         expiryFilter = (Button) findViewById(R.id.expiryDateFilterBtn);
         payoutFilter = (Button) findViewById(R.id.payoutFilterBtn);
+        detailedFilter = (ImageView) findViewById(R.id.detailedFilterBtn);
 
         brandFilter.setOnClickListener(this);
         expiryFilter.setOnClickListener(this);
         payoutFilter.setOnClickListener(this);
+        detailedFilter.setOnClickListener(this);
 
+        categories = new String[]{"Lifestyle", "Sports", "Service", "Fashion"};
+        categoryIndexes = new Integer[]{0, 1, 2, 3};
+        checkedCategories = new boolean[categories.length];
+        for (int i = 0; i < categories.length; i++) {
+            checkedCategories[i] = true;
+        }
+        userSelectedSurveys = new ArrayList<Integer>(Arrays.asList(categoryIndexes));
     }
 
     private void search(String str) {
         ArrayList<Survey> list = new ArrayList<>();
-        for (Survey s : surveys) {
+        for (Survey s : originalSurveys) {
             if (s.getName().toLowerCase().contains(str.toLowerCase())) {
                 list.add(s);
             }
         }
+        currSurveys = cloneArray(list);
         SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(list);
         recyclerView.setAdapter(surveyAdapterClass);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.brandNameFilterBtn) {
+        if (v.getId() == R.id.detailedFilterBtn) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(SurveysHomeActivity.this);
+            mBuilder.setTitle("Select Brand Categories");
+            mBuilder.setMultiChoiceItems(categories, checkedCategories, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+                    if (isChecked) {
+                        if (!userSelectedSurveys.contains((Integer)position)) {
+                            userSelectedSurveys.add((Integer)position);
+                        } else {
+                            userSelectedSurveys.remove((Integer)position);
+                        }
+                    } else {
+                        if (userSelectedSurveys.contains((Integer)position)) {
+                            userSelectedSurveys.remove((Integer)position);
+                        }
+                    }
+                }
+            });
+            mBuilder.setCancelable(false);
+            mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ArrayList<Survey> selectedSurveys = new ArrayList<>();
+                    for (Integer i : userSelectedSurveys) {
+                        String cat = categories[i];
+                        for (Survey s : originalSurveys) {
+                            if (s.getCategories().contains(cat) && !selectedSurveys.contains(s)) {
+                                selectedSurveys.add(s);
+                            }
+                        }
+                    }
+                    currSurveys = cloneArray(selectedSurveys);
+                    SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(selectedSurveys);
+                    recyclerView.setAdapter(surveyAdapterClass);
+                }
+            });
+            mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            mBuilder.setNeutralButton("Select All", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    for (int i = 0; i < checkedCategories.length; i++) {
+                        checkedCategories[i] = true;
+                        userSelectedSurveys = new ArrayList<Integer>(Arrays.asList(categoryIndexes));
+                        currSurveys = cloneArray(originalSurveys);
+                        SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(originalSurveys);
+                        recyclerView.setAdapter(surveyAdapterClass);
+                    }
+                }
+            });
+            AlertDialog mDialog = mBuilder.create();
+            mDialog.show();
+        } else if (v.getId() == R.id.brandNameFilterBtn) {
             if (BRAND_FILTER_STATE == 0) {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         return s1.getBrand().compareTo(s2.getBrand());
                     }
                 });
             } else {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         return s2.getBrand().compareTo(s1.getBrand());
                     }
                 });
             }
-            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(surveys);
+            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(currSurveys);
             recyclerView.setAdapter(surveyAdapterClass);
             BRAND_FILTER_STATE = 1 - BRAND_FILTER_STATE;
         } else if (v.getId() == R.id.expiryDateFilterBtn) {
             if (EXPIRY_FILTER_STATE == 0) {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         return s1.getExpiryDF().compareTo(s2.getExpiryDF());
                     }
                 });
             } else {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         return s2.getExpiryDF().compareTo(s1.getExpiryDF());
                     }
                 });
             }
-            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(surveys);
+            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(currSurveys);
             recyclerView.setAdapter(surveyAdapterClass);
             EXPIRY_FILTER_STATE = 1 - EXPIRY_FILTER_STATE;
         } else if (v.getId() == R.id.payoutFilterBtn) {
             if (PAYOUT_FILTER_STATE == 0) {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         if (s1.getCashout() - s2.getCashout() > 0) {
@@ -162,7 +244,7 @@ public class SurveysHomeActivity extends AppCompatActivity implements View.OnCli
                     }
                 });
             } else {
-                Collections.sort(surveys, new Comparator<Survey>() {
+                Collections.sort(currSurveys, new Comparator<Survey>() {
                     @Override
                     public int compare(Survey s1, Survey s2) {
                         if (s2.getCashout() - s1.getCashout() > 0) {
@@ -175,10 +257,18 @@ public class SurveysHomeActivity extends AppCompatActivity implements View.OnCli
                     }
                 });
             }
-            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(surveys);
+            SurveyAdapterClass surveyAdapterClass = new SurveyAdapterClass(currSurveys);
             recyclerView.setAdapter(surveyAdapterClass);
             PAYOUT_FILTER_STATE = 1 - PAYOUT_FILTER_STATE;
         }
+    }
+
+    public ArrayList<Survey> cloneArray (ArrayList<Survey> toClone) {
+        ArrayList<Survey> newList = new ArrayList<>();
+        for (Survey s : toClone) {
+            newList.add(s);
+        }
+        return newList;
     }
 
     public void onPause() {
